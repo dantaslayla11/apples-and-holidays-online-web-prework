2ee490c3e6c5bff72b5e3dc2126a4653934d0cdf
layla dantas <laylatdantas@gmail.com>
2019-05-28 00:41:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
    holiday_hash.each do |k,v|
-     puts "#{k}".capitalize()
+     puts "#{k}".capitalize() + ":"
   end
   
   # iterate through holiday_hash and print items such that your readout resembles:
