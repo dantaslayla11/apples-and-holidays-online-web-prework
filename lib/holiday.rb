6fc8c5682f82f8830754ba8922ac78ca77b95826
layla dantas <laylatdantas@gmail.com>
2019-05-28 01:25:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -111,7 +111,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   
   puts holiday_hash
-  array[]
+  
   
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
