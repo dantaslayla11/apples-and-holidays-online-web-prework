553d5e0d8e9ddc47367eb83de9d048d080d4ec92
layla dantas <laylatdantas@gmail.com>
2019-05-28 00:44:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_supplies_in_holidays(holiday_hash)
      puts "#{k}".capitalize() + ":"
       v.each do |attribute,value|
         attribute.capitalize() + ":"
-        
+      end  
   end
   
   
