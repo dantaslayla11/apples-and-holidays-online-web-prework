e8a6320a09dfd58d844fad0c96d2a950a6a5a211
layla dantas <laylatdantas@gmail.com>
2019-05-28 01:01:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,14 +75,13 @@ end
 def all_supplies_in_holidays(holiday_hash)
     puts  "#{k}".capitalize() + ":"
      v.each do |k1,v1|
-      k1.collect do |item|
-        item.join("_").capitalize()
-      end  
+      
       v1.collect do |item|
         item.capitalize()
       end
       
       string = v1.join(", ")
+      string = k1.join("_")
       puts "  " + "#{k1}".capitalize() + ": " + "#{string}"
 end      
   
